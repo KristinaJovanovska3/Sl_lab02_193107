73b600af8685283a016aa5695cb90162fe0bb676
Jovanovska <kjovanovska27@gmail.com>
2021-05-31 18:19:43 +0200

    dd

---
+++ src/main/java/SI_Lab2.java
@@ -0,0 +1,36 @@
+import java.util.*;
+public class SI_Lab2 {
+      public static List<Integer> function(List<Time> timesList) {
+            List<Integer> result = new ArrayList<>();
+            //17:32:60
+            for (int i = 0; i < timesList.size(); i++) {  //---------- a
+                int hr = timesList.get(i).getHours();
+                int min = timesList.get(i).getMinutes();
+                int sec = timesList.get(i).getSeconds();
+                if (hr < 0 || hr > 24) { //-------- b
+                    if (hr < 0) {  //------- c
+                        throw new RuntimeException("The hours are smaller than the minimum"); //--------- d
+                    } else {//-------- e
+                        throw new RuntimeException("The hours are grater than the maximum");
+                    }
+                } else if (hr < 24) {//------- f
+                    if (min < 0 || min > 59) //------- g
+                        throw new RuntimeException("The minutes are not valid!");//--------- h
+                    else {//---------- i
+                        if (sec >= 0 && sec <= 59) //--------- j
+                            result.add(hr * 3600 + min * 60 + sec); //---------- k
+                        else//------------ l
+                            throw new RuntimeException("The seconds are not valid");
+                    }
+                } else if (hr == 24 && min == 0 && sec == 0) {//------------- m
+                    result.add(hr * 3600 + min * 60 + sec);//--------- n
+                } else {    //------ o
+                    throw new RuntimeException("The time is greater than the maximum");
+                }
+            }
+
+            return result;  //-------- p
+        } //-------- q
+    }
+
+
